ac2b44e54a782564ace140bb6fc0a9f8cfd4cccb
Beryl Armand <beryl.armand@gmail.com>
2021-03-04 11:52:32 +0100

    Seed with 100 ingredients

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_04_100458) do
+ActiveRecord::Schema.define(version: 2021_03_04_101909) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
