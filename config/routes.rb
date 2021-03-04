24f1d219ec4ebaf7417ace90943639625eb5d32b
Beryl Armand <beryl.armand@gmail.com>
2021-03-04 13:50:03 +0100

    Cocktails routes, actions and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root to: "cocktails#index" 
+  resources :cocktails, only: [:show, :new, :create]
+
+    # A user can see the list of cocktails
+    # GET "cocktails" => index
+    # A user can see the details of a given cocktail, with the dose needed for each ingredient
+    # GET "cocktails/42" => show
+    # A user can create a new cocktail
+    # GET "cocktails/new" => new
+    # POST "cocktails" => create
+
 end
